f553baa6343c63bd1044c4b38bf020962bf9882d
Bagus Aji <baguztaji07@gmail.com>
2022-08-23 03:33:14 +0700

    add export function

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('tokopedia/', views.tokopedia, name='tokopedia'),
     path('bukalapak/', views.bukalapak, name='bukalapak'),
+    path('tokopedia/download/', views.tokopedia_download, name='tokopedia_download'),
+    path('bukalapak/download/', views.bukalapak_download, name='bukalapak_download'),
     # path('delete/<int:id>', views.delete, name='delete'),
     # path('update/<int:id>', views.update, name='update'),
     # path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+import csv
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
@@ -6,6 +7,36 @@ from django.urls import reverse
 from .models import Products
 from .modules.tokoscraper import tokopedia_main
 from .modules.bukascraper import bukalapak_main
+
+def tokopedia_download(request):
+    if request.method == 'POST':
+        total = request.POST['amount']
+        # Create the HttpResponse object with the appropriate CSV header.
+        response = HttpResponse(
+            content_type='text/csv',
+            headers={'Content-Disposition': 'attachment; filename="tokoscraper_tokopedia.csv"'},
+        )
+        pdatas = tokopedia_main(total)
+        writer = csv.writer(response)
+        writer.writerow(['Name', 'Price', 'Store Name', 'Rating', 'Review', 'URL'])
+        for data in pdatas:
+            writer.writerow([data['name'], data['price'], data['shop']['name'], data['rating'], data['countReview'], data['url']])
+        return response
+
+def bukalapak_download(request):
+    if request.method == 'POST':
+        total = request.POST['amount']
+        # Create the HttpResponse object with the appropriate CSV header.
+        response = HttpResponse(
+            content_type='text/csv',
+            headers={'Content-Disposition': 'attachment; filename="tokoscraper_bukalapak.csv"'},
+        )
+        pdatas = bukalapak_main(total)
+        writer = csv.writer(response)
+        writer.writerow(['Name', 'Price', 'Store Name', 'Rating', 'Review', 'URL', 'Description'])
+        for data in pdatas:
+            writer.writerow([data['name'], data['price'], data['store']['name'], data['rating']['average_rate'], data['rating']['user_count'], data['url'], data['description']])
+        return response
 
 def tokopedia(request):
     if request.method == 'GET':
